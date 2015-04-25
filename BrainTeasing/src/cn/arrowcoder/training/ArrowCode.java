24c289c478180176d16a50362c9995e8ce6c4a2f
Jianqing Zhang <jzhang.labs@gmail.com>
2015-04-24 17:50:53 -0700

    minor

--- BrainTeasing/src/cn/arrowcoder/training/ArrowCode.java
+++ BrainTeasing/src/cn/arrowcoder/training/ArrowCode.java
@@ -984,10 +984,11 @@ public class ArrowCode {
      * Divide Two Integers 
      */
     public int divide(int dividend, int divisor) {
-    	if (divisor == 0) return Integer.MIN_VALUE;
+    	if (divisor == 0) return Integer.MAX_VALUE;
     	int sign =(dividend > 0 && divisor < 0 || dividend < 0 && divisor > 0) ? -1 : 1; 
     	//if (dividend == divisor) return 1; // Cover the cast that both numbers are INT_MAX or INT_MIN
-    	if (dividend == Integer.MIN_VALUE && Math.abs(divisor) == 1) return Integer.MAX_VALUE;
+    	if (dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+    	if (dividend == Integer.MIN_VALUE && divisor == 1) return Integer.MIN_VALUE;
     	//if (divisor == Integer.MIN_VALUE) return 0;
     	//if (dividend == Integer.MIN_VALUE)
     	//	dividend++;
