3c2d2dc76e65e9c7dacc61f060544b10445de8e5
arrowcoder <jzhang.labs@gmail.com>
2016-11-29 00:29:16 -0800

    today's last

--- BrainTeasing/src/cn/arrowcoder/training/ArrowLeetCode.java
+++ BrainTeasing/src/cn/arrowcoder/training/ArrowLeetCode.java
@@ -4,5 +4,12 @@ package cn.arrowcoder.training;
  * Leetcode practice for Job 2017 
  */
 public class ArrowLeetCode {
-
+    
+	/**
+	 * P1: two sum
+	 */
+	public boolean twoSum(){
+	    return true;
+	    
+	}
 }
