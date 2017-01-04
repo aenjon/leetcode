df6964782e8cffa24e7a888094f460240145664c
arrowcoder <jzhang.labs@gmail.com>
2017-01-03 17:09:45 -0800

    P020. Valid Parentheses

--- BrainTeasing/src/cn/arrowcoder/training/ArrowLeetCode.java
+++ BrainTeasing/src/cn/arrowcoder/training/ArrowLeetCode.java
@@ -544,6 +544,30 @@ public class ArrowLeetCode {
     }
 
     /**
+     * P020. Valid Parentheses
+     */
+    public boolean p020_isValid(String s) {
+        if (s == null || s.isEmpty())
+            return true;
+        boolean ret = true;
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            } else if (st.isEmpty()) {
+                ret = false;
+            } else {
+                char stack_c = st.pop().charValue();
+                ret = stack_c == '(' && c == ')' || stack_c == '[' && c == ']' || stack_c == '{' && c == '}';
+            }
+            if (!ret)
+                break;
+        }
+        return ret && st.isEmpty();
+    }
+
+    /**
      * P027. Remove Element
      */
     public int p027_removeElement(int[] nums, int val) {
