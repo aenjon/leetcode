11ec9bc0b4f34c83dc378f13c445439411d8cc82
arrowcoder <jzhang.labs@gmail.com>
2017-01-03 17:26:36 -0800

    P021. Merge Two Sorted Lists

--- BrainTeasing/src/cn/arrowcoder/training/ArrowLeetCode.java
+++ BrainTeasing/src/cn/arrowcoder/training/ArrowLeetCode.java
@@ -568,6 +568,30 @@ public class ArrowLeetCode {
     }
 
     /**
+     * P021. Merge Two Sorted Lists
+     */
+    public ListNode p021_mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        } else {
+            cur.next = l2;
+        }
+        return dummy.next;
+    }
+
+    /**
      * P027. Remove Element
      */
     public int p027_removeElement(int[] nums, int val) {
