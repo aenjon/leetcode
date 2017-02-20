2ab16e969276f5f1fb514c923c9032b3d7b53296
arrowcoder <jzhang.labs@gmail.com>
2017-02-20 13:44:07 -0800

    P062. Unique Paths

--- BrainTeasing/src/cn/arrowcoder/training/ArrowLeetCode.java
+++ BrainTeasing/src/cn/arrowcoder/training/ArrowLeetCode.java
@@ -1319,6 +1319,24 @@ public class ArrowLeetCode {
     }
 
     /**
+     * P062. Unique Paths
+     */
+    public int p062_uniquePaths(int m, int n) {
+        int[][] res = new int[m][n];
+        for(int i =0; i< m; i++)
+            for(int j = 0; j < n; j++){
+                if (i == 0){
+                    res[i][j] = 1;
+                }else if (j == 0){
+                    res[i][j] = 1;
+                }else{
+                    res[i][j] = res[i-1][j] + res[i][j-1];
+                }
+            }
+        return res[m - 1][n - 1];
+    }
+
+    /**
      * P069. Sqrt(x)
      */
     public int p069_mySqrt(int x) {
