131e2d0cc60b1e4271f5cfeb12d11d28d43f9af5
arrowcoder <jzhang.labs@gmail.com>
2017-02-21 23:21:16 -0800

    Problem #63 Unique Paths II

--- BrainTeasing/src/cn/arrowcoder/training/ArrowLeetCode.java
+++ BrainTeasing/src/cn/arrowcoder/training/ArrowLeetCode.java
@@ -1323,17 +1323,40 @@ public class ArrowLeetCode {
      */
     public int p062_uniquePaths(int m, int n) {
         int[][] res = new int[m][n];
-        for(int i =0; i< m; i++)
-            for(int j = 0; j < n; j++){
-                if (i == 0){
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
                     res[i][j] = 1;
-                }else if (j == 0){
+                } else if (j == 0) {
                     res[i][j] = 1;
-                }else{
-                    res[i][j] = res[i-1][j] + res[i][j-1];
+                } else {
+                    res[i][j] = res[i - 1][j] + res[i][j - 1];
                 }
             }
         return res[m - 1][n - 1];
+    }
+
+    /**
+     * P063. Unique Paths II
+     */
+    public int p063_uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        obstacleGrid[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                if (obstacleGrid[i][j] == 1) {
+                    obstacleGrid[i][j] = 0;
+                } else if (i == 0 && j > 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i][j - 1];
+                } else if (j == 0 && i > 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j];
+                } else {
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+                }
+            }
+        return obstacleGrid[m - 1][n - 1];
     }
 
     /**
