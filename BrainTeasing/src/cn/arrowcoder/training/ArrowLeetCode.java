f01a856883d516f83506baa23eed22b9e9a8e1f5
arrowcoder <jzhang.labs@gmail.com>
2017-02-21 23:53:20 -0800

    P064. Minimum Path Sum

--- BrainTeasing/src/cn/arrowcoder/training/ArrowLeetCode.java
+++ BrainTeasing/src/cn/arrowcoder/training/ArrowLeetCode.java
@@ -1348,15 +1348,35 @@ public class ArrowLeetCode {
                     continue;
                 if (obstacleGrid[i][j] == 1) {
                     obstacleGrid[i][j] = 0;
-                } else if (i == 0 && j > 0) {
+                } else if (i == 0) {
                     obstacleGrid[i][j] = obstacleGrid[i][j - 1];
-                } else if (j == 0 && i > 0) {
+                } else if (j == 0) {
                     obstacleGrid[i][j] = obstacleGrid[i - 1][j];
                 } else {
                     obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
                 }
             }
         return obstacleGrid[m - 1][n - 1];
+    }
+
+    /**
+     * P064. Minimum Path Sum
+     */
+    public int p064_minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                if (i == 0) {
+                    grid[i][j] += grid[i][j - 1];
+                } else if (j == 0) {
+                    grid[i][j] += grid[i - 1][j];
+                } else {
+                    grid[i][j] += grid[i][j - 1] < grid[i - 1][j] ? grid[i][j - 1] : grid[i - 1][j];
+                }
+            }
+        return grid[m - 1][n - 1];
     }
 
     /**
